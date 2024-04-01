641e7a5c491589803d4cb283f1adc1fad7df4e20
fredo09 <fred_15@outlokk.com>
2024-03-31 22:45:35 -0600

    add component Navbar

--- app/(generalPages)/layout.tsx
+++ app/(generalPages)/layout.tsx
@@ -1,14 +1,21 @@
 
+
+
 /**
  *  estructura layout pages
  * (_NOMBE_FILE_) -> ayuda a que se cargue o agrupe las pages y que no se cuente la carpeta que tiene esa agrupacion. 
  */
 
-export default function RootLayout({ children }: { children: React.ReactNode; }) {
+import { Navbar } from "@/components";
+
+export default function PublicLayout({ children }: { children: React.ReactNode; }) {
   return (
-    <main className="flex flex-col items-center p-24">
-      <span className="text-lg"> hola mundo</span>
-      { children }
-    </main>
+    <>
+      <Navbar />
+      <main className="flex flex-col items-center p-24">
+        <span className="text-lg"> hola mundo</span>
+        {children}
+      </main>
+    </>
   );
 }
---
+++ components/index.ts
@@ -0,0 +1,5 @@
+/**
+ * export components
+*/
+
+export * from './navbar/Navbar';
