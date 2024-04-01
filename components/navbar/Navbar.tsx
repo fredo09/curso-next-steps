3ca691b09f3f273e654934edcc3db9e76c6b79ae
fredo09 <fred_15@outlokk.com>
2024-03-31 23:48:25 -0600

    update navbar component

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import { HomeIcon } from "@primer/octicons-react";
+import Link from "next/link";
+import React from "react";
+
+type navPaths = [
+  { path: string; value: string },
+  { path: string; value: string },
+  { path: string; value: string }
+];
+
+const navItems: navPaths = [
+  { path: "/about", value: "About" },
+  { path: "/pricing", value: "pricing" },
+  { path: "/contact", value: "Contact" },
+];
 
 export const Navbar = () => {
-    console.log("🚀 ~ Navbar ~ Navbar:", Navbar);
+  return (
+    <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
+      <Link href={"/"} className="flex items-center">
+        <HomeIcon className="mr-2" />
+        <span>Home</span>
+      </Link>
+      <div className="flex-1"></div>
 
-    return (
-        <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-            <span>Home</span>
-            <div className="flex-1"></div>
-
-            <a className='mr-2' href="/contact">Contact</a>
-            <a className='mr-2' href="/about">About</a>
-            <a className='mr-2' href="/pricing">Pricing</a>
-        </nav>
-    );
-}
+      {navItems.map((item) => (
+        <Link key={item.path} className="mr-2" href={item.path}>
+          {item.value}
+        </Link>
+      ))}
+    </nav>
+  );
+};
